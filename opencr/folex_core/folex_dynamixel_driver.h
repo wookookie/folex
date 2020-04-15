c320a31e034bc1a95b54a7a846fafa669f2f1fec
danichoi737 <danichoi737@gmail.com>
2020-04-15 13:11:01 +0900

    Redefine Dynamixel ID

--- opencr/folex_core/folex_dynamixel_driver.h
+++ opencr/folex_core/folex_dynamixel_driver.h
@@ -27,6 +27,27 @@
 #define LEN_AX_GOAL_POSITION      2
 #define LEN_AX_MOVING_SPEED       2
 
+/* DYNAMIXEL ID */
+// FRONT-LEFT
+#define JOINT_1                   1           // XL430-W250-T
+#define JOINT_5                   5           // AX-12A
+#define JOINT_9                   9           // AX-12A
+
+// FRONT-RIGHT
+#define JOINT_2                   2           // XL430-W250-T
+#define JOINT_6                   6           // AX-12A
+#define JOINT_10                  10          // AX-12A
+
+// REAR-LEFT
+#define JOINT_3                   3           // XL430-W250-T
+#define JOINT_7                   7           // AX-12A
+#define JOINT_11                  11          // AX-12A
+
+// REAR-RIGHT
+#define JOINT_4                   4           // XL430-W250-T
+#define JOINT_8                   8           // AX-12A
+#define JOINT_12                  12          // AX-12A
+
 
 // Dynamixel parameters
 #define BAUDRATE                  1000000
@@ -36,11 +57,6 @@
 // Dynamixel torque control
 #define TORQUE_DISABLE            0
 #define TORQUE_ENABLE             1
-
-// Dynamixel ID
-#define JOINT_1                   1           // XL430-W250-T
-#define JOINT_2                   2           // AX-12A
-#define JOINT_3                   3           // AX-12A
 
 // Test
 #define XL_TEST_POSITION          2048
@@ -63,9 +79,10 @@ private:
   float protocol_version_;
   bool torque_;
 
-  uint8_t joint_1_id_;
-  uint8_t joint_2_id_;
-  uint8_t joint_3_id_;
+  uint8_t joint_1_id_, joint_5_id_, joint_9_id_;
+  uint8_t joint_2_id_, joint_6_id_, joint_10_id_;
+  uint8_t joint_3_id_, joint_7_id_, joint_11_id_;
+  uint8_t joint_4_id_, joint_8_id_, joint_12_id_;
 
   dynamixel::PortHandler *portHandler_;
   dynamixel::PacketHandler *packetHandler_;
